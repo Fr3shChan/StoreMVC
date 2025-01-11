c785d8c24e051ca2cbc3ded52f7a01391120c32c
Marcin Gierszewski <mgierowski61@gmail.com>
2025-01-11 20:08:56 +0100

    Dodaj pliki projektów.

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreMVC.Models;
+
+namespace StoreMVC.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        { 
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
